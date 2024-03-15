9bc9281c4783a3b43022a299c62a79530c5a4174
Varnoi <159175375+Varnoi@users.noreply.github.com>
2024-03-15 11:03:47 +0300

    123

---
+++ kr1/1.js
@@ -0,0 +1,8 @@
+function TriangleArea(base = 7, height = 3) {
+    var area = 0.5 * base * height;
+    console.log("Площа трикутника з основою " + base + " і висотою " + height + " дорівнює " + area);
+    return area;
+}
+
+TriangleArea(3, 6);
+TriangleArea();
